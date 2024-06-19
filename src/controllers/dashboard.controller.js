164286c1df472fbc5ed114f3df342956a35f9a01
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-20 01:22:23 +0530

    getChannel

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose"
 
+const getChannelStats = asyncHandler(async (req, res) => {
+    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+})
 
 export {
     getChannelStats, 
