2a7f01035a95e47b983bf0ac5706a0c390853f8b
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-20 01:27:45 +0530

    import

---
+++ src/controllers/playlist.controller.js
@@ -0,0 +1,15 @@
+import mongoose, {isValidObjectId} from "mongoose"
+
+
+
+const createPlaylist = asyncHandler(async (req, res) => {
+    const {name, description} = req.body
+
+    //TODO: create playlist
+})
+
+
+export {
+    createPlaylist,
+    
+}
