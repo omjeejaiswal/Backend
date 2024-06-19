ff07600566cf2c8b404533e3d78e916f6d725750
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-20 01:23:46 +0530

    heleathcheck

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,6 +1,11 @@
 import {ApiError} from "../utils/ApiError.js"
 
 
+
+const healthcheck = asyncHandler(async (req, res) => {
+    //TODO: build a healthcheck response that simply returns the OK status as json with a message
+})
+
 export {
     healthcheck
     }
