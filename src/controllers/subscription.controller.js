4fc173c3c748e871cf0eb6897100ac7271a59a84
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-20 01:33:39 +0530

    subscription

---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,14 @@
+import mongoose, {isValidObjectId} from "mongoose"
+import {User} from "../models/user.model.js"
+import { Subscription } from "../models/subscription.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+
+
+export {
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
+}
