18444d3c735c51f9d2f201cb44c8e2c76a2c5f27
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-20 01:22:42 +0530

    getChannel

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -4,6 +4,10 @@ const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
 })
 
+const getChannelVideos = asyncHandler(async (req, res) => {
+    // TODO: Get all the videos uploaded by the channel
+})
+
 export {
     getChannelStats, 
     getChannelVideos
