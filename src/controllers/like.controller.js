7391bd85522a0f3cae3d79ad98f3bdc9dfca533c
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-20 01:25:51 +0530

    tooglevideo

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -4,6 +4,10 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
+const toggleVideoLike = asyncHandler(async (req, res) => {
+    const {videoId} = req.params
+    //TODO: toggle like on video
+})
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
