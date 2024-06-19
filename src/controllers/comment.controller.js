84b763120e62d3fe6c5fb4fc9996681c09bc7cce
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-20 01:19:57 +0530

    getvideoComment

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -4,3 +4,10 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
+const getVideoComments = asyncHandler(async (req, res) => {
+    //TODO: get all comments for a video
+    const {videoId} = req.params
+    const {page = 1, limit = 10} = req.query
+
+})
+
